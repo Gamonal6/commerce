f93189233bf01545329208af52118a2e8f652600
Gamonal6 <henriquegamonal3@gmail.com>
2022-04-24 11:57:16 -0300

    new commit

--- auctions/models.py
+++ auctions/models.py
@@ -4,3 +4,11 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class auctions(models.Model):
+    title = models.CharField(max_length=30)
+    bids = models.IntegerField()
+    description = models.TextField(max_length=400)
+    img_url = models.CharField(max_length=200)
+    category = models.CharField(max_length=20)
